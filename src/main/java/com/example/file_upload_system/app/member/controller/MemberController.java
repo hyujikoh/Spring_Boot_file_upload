7c544c2bdb39b16ef69af65a6990b38a7e31f81d
hyujikoh <dvum0045@gmail.com>
2022-09-16 01:20:41 +0900

    프로필이미지URL을 깔끔하게 줄이기, HTTP 상태코드 302 REDIRECT 사용

--- src/main/java/com/example/file_upload_system/app/member/controller/MemberController.java
+++ src/main/java/com/example/file_upload_system/app/member/controller/MemberController.java
@@ -11,6 +11,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.multipart.MultipartFile;
@@ -70,5 +71,11 @@ public class MemberController {
     @GetMapping("/login")
     public String showLogin() {
         return "member/login";
+    }
+
+
+    @GetMapping("/profile/img/{id}")
+    public String showProfileImg(@PathVariable Long id) {
+        return "redirect:" + memberService.getMemberById(id).getProfileImgUrl();
     }
 }
