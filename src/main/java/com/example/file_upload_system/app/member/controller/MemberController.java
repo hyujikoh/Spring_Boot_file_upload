2d87985686d8c83c84ccd09b463d0c00f528a03f
hyujikoh <dvum0045@gmail.com>
2022-09-16 01:04:04 +0900

    MemberContext에 email필드 추가하여, 프로필페이지에서 발생하는 쿼리 줄이기

--- src/main/java/com/example/file_upload_system/app/member/controller/MemberController.java
+++ src/main/java/com/example/file_upload_system/app/member/controller/MemberController.java
@@ -3,8 +3,10 @@ package com.example.file_upload_system.app.member.controller;
 
 import com.example.file_upload_system.app.member.entity.Member;
 import com.example.file_upload_system.app.member.service.MemberService;
+import com.example.file_upload_system.app.security.dto.MemberContext;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -59,13 +61,8 @@ public class MemberController {
 
     @PreAuthorize("isAuthenticated()")
     @GetMapping("/profile")
-    public String showProfile(Principal principal, Model model) {
-
-
-        Member loginedMember = memberService.getMemberByUsername(principal.getName());
-
-        model.addAttribute("loginedMember", loginedMember);
-
+    public String showProfile(@AuthenticationPrincipal MemberContext memberContext, Model model) {
+        model.addAttribute("memberContext", memberContext);
         return "member/profile";
     }
 
--- src/main/java/com/example/file_upload_system/app/security/dto/MemberContext.java
+++ src/main/java/com/example/file_upload_system/app/security/dto/MemberContext.java
@@ -10,11 +10,13 @@ import java.util.List;
 @Getter
 public class MemberContext extends User {
     private final Long id;
+    private final String email;
     private final String profileImgUrl;
     // 엔티티는 더 붙여도 된다 .
     public MemberContext(Member member, List<GrantedAuthority> authorities) {
         super(member.getUsername(), member.getPassword(), authorities);
         this.id = member.getId();
         this.profileImgUrl = member.getProfileImgUrl();
+        this.email = member.getEmail();
     }
 }
